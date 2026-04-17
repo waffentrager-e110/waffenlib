24e5a05beb6389fe0923edda7d1dad39441288b0
waffentrager-e110 <piskarevleonard@gmail.com>
2026-04-17 23:35:32 +0700

    Update __init__.py

--- src/waffenlib/__init__.py
+++ src/waffenlib/__init__.py
@@ -2,7 +2,7 @@ from endstone.plugin import Plugin
 
 class WaffenLib(Plugin):
     def on_enable(self) -> None:
-        self.get_logger().info("§aWaffenLib v0.1.0 загружена!")
+        self.logger.info("§aWaffenLib v0.1.0 загружена!")
 
     def on_disable(self) -> None:
-        self.get_logger().info("§cWaffenLib выгружена.")
+        self.logger.info("§cWaffenLib выгружена.")
